2de9ede620aecb042e02cc3764901ef7cba51073
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-10-30 19:45:30 +0200

    Cleanup of ObjectExtensions
    Also optimized it

--- Freecam/ObjectExtensions.cs
+++ Freecam/ObjectExtensions.cs
@@ -1,37 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 
 namespace FreeCamMain
 {
     internal static class ObjectExtensions
     {
-        public static GameObject GetPlayerCamera
-        {
-            get
-            {
-                if (CachedPlayerCamera == null)
-                {
-                    CachedPlayerCamera = GameObject.Find("Camera (eye)");
-                }
-                return CachedPlayerCamera;
-            }
-        }
-
         public static GameObject CreateCamera(float fov, bool forwards, float DistanceMultiplier = 2f, bool AttachToPlayer = true)
         {
-            GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Object.Destroy(gameObject.GetComponent<MeshRenderer>());
-            gameObject.transform.localScale = Camera.transform.localScale;
+            GameObject gameObject;
+
+            if (AttachToPlayer)
+            {
+                gameObject = new GameObject
+                {
+                    transform =
+                    {
+                        localScale = Camera.transform.localScale,
+                        parent = Camera.transform
+                    }
+                };
+            }
+            else
+            {
+                gameObject = new GameObject
+                {
+                    transform =
+                    {
+                        localScale = Camera.transform.localScale
+                    }
+                };
+            }
+            
             Rigidbody rigidbody = gameObject.AddComponent<Rigidbody>();
             rigidbody.isKinematic = true;
             rigidbody.useGravity = false;
-            gameObject.GetComponent<Collider>().enabled = false;
-            gameObject.GetComponent<Renderer>().enabled = false;
             Camera camera = gameObject.AddComponent<Camera>();
             camera.nearClipPlane = 0.01f;
-            if (AttachToPlayer)
-            {
-                gameObject.transform.parent = Camera.transform;
-            }
             gameObject.transform.SetPositionAndRotation(Camera.transform.position, Camera.transform.rotation);
             if (forwards)
             {
@@ -43,28 +47,11 @@ namespace FreeCamMain
                 gameObject.transform.position += -gameObject.transform.forward * DistanceMultiplier;
             }
             Camera.enabled = false;
-            Camera component = gameObject.GetComponent<Camera>();
-            component.fieldOfView = fov;
-            component.enabled = false;
+            camera.fieldOfView = fov;
+            camera.enabled = false;
             return gameObject;
         }
 
-        private static Camera Camera
-        {
-            get
-            {
-                return CameraObject.GetComponent<Camera>();
-            }
-        }
-
-        private static GameObject CameraObject
-        {
-            get
-            {
-                return GetPlayerCamera;
-            }
-        }
-
-        private static GameObject CachedPlayerCamera;
+        public static Camera Camera => Camera.main;
     }
 }
